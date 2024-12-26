92021cd36a795e37fcf1ce8f24b697a2d9cb06d8
onleep <120320355+onleep@users.noreply.github.com>
2024-12-26 13:30:07 +0300

    Fix predict price bugs

--- app/fastapi.py
+++ app/fastapi.py
@@ -1,16 +1,14 @@
-from fastapi import FastAPI, File, UploadFile
-import uvicorn
+from fastapi import FastAPI, Request
 import pandas as pd
+import uvicorn
 import json
-import io
-import numpy as np
 
 app = FastAPI()
 
-@app.post("/process-json/")
-async def process_json(file: UploadFile = File(...)):
-    # Шаг 1: Чтение JSON
-    contents = await file.read()
+
+@app.post('/predict')
+async def process_json(request: Request):
+    contents = await request.body()
     data = json.loads(contents)
 
     # Шаг 2: Создание таблиц
@@ -39,68 +37,81 @@ async def process_json(file: UploadFile = File(...)):
     realty_inside_df = pd.DataFrame(tables["realty_inside"])
     realty_outside_df = pd.DataFrame(tables["realty_outside"])
 
-    addresses_df['lat'] = addresses_df['coordinates'].apply(lambda x: x['lat'] if isinstance(x, dict) else None)
-    addresses_df['lng'] = addresses_df['coordinates'].apply(lambda x: x['lng'] if isinstance(x, dict) else None)
+    addresses_df['lat'] = addresses_df['coordinates'].apply(
+        lambda x: x['lat'] if isinstance(x, dict) else None)
+    addresses_df['lng'] = addresses_df['coordinates'].apply(
+        lambda x: x['lng'] if isinstance(x, dict) else None)
     addresses_df.drop(columns=['coordinates', 'address'], inplace=True)
 
     offers_df = offers_df.dropna(subset=['photos_count'])
-    offers_df['publication_at'] = pd.to_datetime(pd.to_datetime(offers_df['publication_at'], unit = 's', utc = True).dt.date)
+    offers_df['publication_at'] = pd.to_datetime(pd.to_datetime(
+        offers_df['publication_at'], unit='s', utc=True).dt.date)
 
     offers_df = offers_df.dropna(subset=['photos_count'])
-    offers_df['publication_at'] = pd.to_datetime(pd.to_datetime(offers_df['publication_at'], unit = 's', utc = True).dt.date)
-    
-    realty_details_df['finish_year'] = realty_details_df['finish_date'].apply(lambda x: x.get('year') if isinstance(x, dict) else None)
+    offers_df['publication_at'] = pd.to_datetime(pd.to_datetime(
+        offers_df['publication_at'], unit='s', utc=True).dt.date)
+
+    realty_details_df['finish_year'] = realty_details_df['finish_date'].apply(
+        lambda x: x.get('year') if isinstance(x, dict) else None)
 
     realty_details_df['realty_type'] = realty_details_df['realty_type'].replace('none', None)
-    realty_details_df['heat_type'] = realty_details_df['heat_type'].replace('none', None)
     realty_details_df.loc[realty_details_df['finish_year'] <= 0, 'finish_year'] = None
     realty_outside_df['material_type'] = realty_outside_df['material_type'].replace('none', None)
-    realty_outside_df['material_type'] = realty_outside_df['parking_type'].replace('none', None)
-    realty_details_df.drop(columns = ['finish_date'], inplace = True)
+    realty_details_df.drop(columns=['finish_date'], inplace=True)
 
-    main_df = addresses_df.merge(offers_df, on='cian_id', how='inner').merge(offers_details_df, on='cian_id', how='inner')
+    main_df = addresses_df.merge(offers_df, on='cian_id', how='inner').merge(
+        offers_details_df, on='cian_id', how='inner')
 
-    tables_to_left_join = [developers_df, realty_details_df, realty_inside_df, realty_outside_df]
+    tables_to_left_join = [developers_df, realty_details_df,
+                           realty_inside_df, realty_outside_df]
     for table in tables_to_left_join:
-            main_df = main_df.merge(table, on='cian_id', how='left')
-    
+        main_df = main_df.merge(table, on='cian_id', how='left')
+
     main_df = main_df[main_df['photos_count'] >= 0].reset_index(drop=True)
 
-    main_df['loggias'] = main_df['loggias'].replace({np.nan: 0})
-    main_df['separated_wc'] = main_df['separated_wc'].replace({np.nan: 0})
-    main_df['balconies'] = main_df['balconies'].replace({np.nan: 0})
-    main_df['combined_wc'] = main_df['combined_wc'].replace({np.nan: 0})
-    main_df['passenger_lifts'] = main_df['passenger_lifts'].replace({np.nan: 0})
+    main_df['separated_wc'] = main_df['separated_wc'].fillna(0)
+    main_df['loggias'] = main_df['loggias'].fillna(0)
+    main_df['balconies'] = main_df['balconies'].fillna(0)
+    main_df['combined_wc'] = main_df['combined_wc'].fillna(0)
+    main_df['passenger_lifts'] = main_df['passenger_lifts'].fillna(0)
 
     main_df['total_rate'] = main_df['total_rate'].fillna(4.180479743602584)
     main_df['review_count'] = main_df['review_count'].fillna(1248.7316089939407)
     main_df['ceiling_height'] = main_df['ceiling_height'].fillna(2.961070749446198)
 
     mean_proportion_ceiling_height = 0.06049278161032404
-    main_df.loc[main_df['ceiling_height'] <= 0, 'ceiling_height'] = main_df['total_area'] * mean_proportion_ceiling_height
+    formula = main_df['total_area'] * mean_proportion_ceiling_height
+    main_df['ceiling_height'] = main_df['ceiling_height'].where(
+        main_df['ceiling_height'] > 0, formula)
 
     mean_proportion_living_area = 0.5486437974462534
-    main_df['living_area'] = main_df['living_area'].fillna(main_df['total_area'] * mean_proportion_living_area)
-
+    main_df['living_area'] = main_df['living_area'].fillna(
+        main_df['total_area'] * mean_proportion_living_area)
 
     mean_proportion_kitchen_area = 0.4577734591774278
-    main_df['kitchen_area'] = main_df['kitchen_area'].fillna((main_df['total_area'] - main_df['living_area']).replace(0, np.nan) * mean_proportion_kitchen_area)
-
+    mask = (main_df['total_area'] - main_df['living_area']
+            ).replace(0, pd.NA) * mean_proportion_kitchen_area
+    main_df['kitchen_area'] = main_df['kitchen_area'].fillna(mask)
 
     mean_proportion_rooms_count = 0.06657494706605477
-    main_df['rooms_count'] = main_df['rooms_count'].fillna(main_df['living_area']*mean_proportion_rooms_count).astype(int)
-    main_df['build_year'] = main_df.apply(lambda row: row['finish_year'] if pd.isna(row['build_year']) else row['build_year'], axis=1)
+    main_df['rooms_count'] = main_df['rooms_count'].fillna(
+        main_df['living_area'] * mean_proportion_rooms_count).astype(int)
+    main_df['build_year'] = main_df.apply(lambda row: row['finish_year'] if pd.isna(
+        row['build_year']) else row['build_year'], axis=1)
 
-    main_df = main_df.dropna(subset=['travel_time', 'views_count', 'kitchen_area', 'build_year']).copy()
-    main_df.drop(columns=['entrances', 'cargo_lifts', 'foundation_year', 'buildings_count', 'lifts_count', 'finish_year'], inplace=True)
+    main_df = main_df.dropna(
+        subset=['travel_time', 'views_count', 'kitchen_area', 'build_year']).copy()
+    main_df.drop(columns=['entrances', 'cargo_lifts', 'foundation_year',
+                 'buildings_count', 'lifts_count', 'finish_year'], inplace=True)
 
     main_df['is_penthouse'] = main_df['is_penthouse'].astype(bool).fillna(False)
     main_df['garbage_chute'] = main_df['garbage_chute'].astype(bool).fillna(False)
-    main_df['is_reliable'] = main_df['is_reliable'].astype(bool).fillna(False)
     main_df['is_emergency'] = main_df['is_emergency'].astype(bool).fillna(False)
     main_df['is_apartment'] = main_df['is_apartment'].astype(bool).fillna(False)
-    main_df['is_mortgage_allowed'] = main_df['is_mortgage_allowed'].astype(bool).fillna(False)
-    main_df['renovation_programm'] = main_df['renovation_programm'].astype(bool).fillna(False)
+    main_df['is_mortgage_allowed'] = main_df['is_mortgage_allowed'].astype(
+        bool).fillna(False)
+    main_df['renovation_programm'] = main_df['renovation_programm'].astype(
+        bool).fillna(False)
 
     main_df['photos_count'] = main_df['photos_count'].astype(int)
     main_df['price'] = main_df['price'].astype(int)
@@ -113,11 +124,8 @@ async def process_json(file: UploadFile = File(...)):
     main_df['passenger_lifts'] = main_df['passenger_lifts'].astype(int)
     main_df['review_count'] = main_df['review_count'].astype(int)
     main_df['build_year'] = main_df['build_year'].astype(int)
-    mask = main_df['project_type'] != 'Индивидуальный проект'
-
-    main_df.loc[mask, 'material_type'] = main_df.loc[mask].groupby('project_type')['material_type'].transform(lambda x: x.ffill().bfill()) 
-    main_df.loc[:, 'material_type'] = main_df['material_type'].fillna(main_df['material_type'].mode()[0])    
-    main_df.loc[:, 'repair_type'] = main_df['repair_type'].fillna(main_df['repair_type'].mode()[0])
+    if main_df['material_type'] is None: main_df['material_type'] = 'ground'
+    if main_df['repair_type'] is None: main_df['repair_type'] = 'cosmetic'
 
     main_df['publication_at'] = pd.to_datetime(main_df['publication_at'])
     main_df['year'] = main_df['publication_at'].dt.year
@@ -127,15 +135,18 @@ async def process_json(file: UploadFile = File(...)):
 
     main_df = main_df.drop(columns=['publication_at'])
 
-    main_df = main_df.applymap(lambda x: str(x) if isinstance(x, list) else x)
-    X = main_df.drop(columns=['price'])
-    # Шаг 5: Подготовка данных для возврата в ответе
-    response = {
-        "X": X.to_dict(orient="records"),
-    }
+    main_df = main_df.drop(columns=['is_reliable', 'heat_type', 'name', 'gas_type', 'parking_type', 'windows_view',
+                           'street', 'agent_name', 'house', 'is_duplicate', 'cian_id', 'lat', 'lng', 'price_changes', 'description'], axis=1)
+    if main_df[['district', 'county', 'sale_type']].isna().any().any():
+        raise Exception
 
-    return {"message": 'IZI', "X_data" : response}
+    X = main_df.drop(columns=['price'])
+    response = {
+        "X": X.to_json(orient='records', force_ascii=False),
+    }
+    return {"message": 'IZI', "X_data": response}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host='localhost', port=8000)
+    
