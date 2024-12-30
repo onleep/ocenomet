ecfc6392f3631dec15843d6daba7fbed1d7d719d
onleep <120320355+onleep@users.noreply.github.com>
2024-12-30 11:17:56 +0300

    Update fastapi

--- api/fastapi.py
+++ api/fastapi.py
@@ -1,116 +1,30 @@
+from .preprocess import preparams, preprepict, encoding, prediction
 from fastapi import FastAPI, HTTPException
-from preprocess import preprocess
-from pydantic import BaseModel
+from app.main import apartPage
+from .models import *
 import uvicorn
-
-
-class Offers(BaseModel):
-    cian_id: int
-    price: float
-    category: str | None = None
-    views_count: int | None = None
-    photos_count: int | None = None
-    floor_number: int | None = None
-    floors_count: int | None = None
-    publication_at: int | None = None
-    price_changes: list | None = None
-
-
-class Addresses(BaseModel):
-    cian_id: int
-    county: str | None = None
-    district: str | None = None
-    street: str | None = None
-    house: str | None = None
-    metro: str | None = None
-    travel_type: str | None = None
-    travel_time: int | None = None
-    address: list | None = None
-    coordinates: dict | None = None
-
-
-class RealtyInside(BaseModel):
-    cian_id: int
-    repair_type: str | None = None
-    total_area: float | None = None
-    living_area: float | None = None
-    kitchen_area: float | None = None
-    ceiling_height: float | None = None
-    balconies: int | None = None
-    loggias: int | None = None
-    rooms_count: int | None = None
-    separated_wc: int | None = None
-    combined_wc: int | None = None
-    windows_view: str | None = None
-
-
-class RealtyOutside(BaseModel):
-    cian_id: int
-    build_year: int | None = None
-    entrances: int | None = None
-    material_type: str | None = None
-    parking_type: str | None = None
-    garbage_chute: bool | None = None
-    lifts_count: int | None = None
-    passenger_lifts: int | None = None
-    cargo_lifts: int | None = None
-
-
-class RealtyDetails(BaseModel):
-    cian_id: int
-    realty_type: str | None = None
-    project_type: str | None = None
-    heat_type: str | None = None
-    gas_type: str | None = None
-    is_apartment: bool | None = None
-    is_penthouse: bool | None = None
-    is_mortgage_allowed: bool | None = None
-    is_premium: bool | None = None
-    is_emergency: bool | None = None
-    renovation_programm: bool | None = None
-    finish_date: dict | None = None
-
-
-class OffersDetails(BaseModel):
-    cian_id: int
-    agent_name: str | None = None
-    deal_type: str | None = None
-    flat_type: str | None = None
-    sale_type: str | None = None
-    is_duplicate: bool | None = None
-    description: str | None = None
-
-
-class Developers(BaseModel):
-    cian_id: int
-    name: str | None = None
-    review_count: int | None = None
-    total_rate: float | None = None
-    buildings_count: int | None = None
-    foundation_year: int | None = None
-    is_reliable: bool | None = None
-
-
-class Data(BaseModel):
-    offers: Offers
-    addresses: Addresses
-    realty_inside: RealtyInside
-    realty_outside: RealtyOutside
-    realty_details: RealtyDetails
-    offers_details: OffersDetails
-    developers: Developers
+import re
 
 app = FastAPI()
 
 
-@app.post('/predict')
-async def process_json(request: Data):
-    X_train = preprocess(request)
-    if isinstance(X_train, list):
-        raise HTTPException(status_code=400, detail=f'В объявлении не указаны: {X}')
-    response = X_train.to_json(orient='records', force_ascii=False)
-    return {'message': 'IZI', 'X_data': response}
+@app.get('/getparams')
+async def getparams(url: str):
+    match = re.search(r'flat/(\d{4,})', url)
+    if not match or not (id := match.group(1)):
+        raise HTTPException(status_code=400, detail=f'Неверный формат объявления')
+    data = apartPage([id], dbinsert=0)
+    data = Params(**data)
+    response = preparams(data)
+    return response.iloc[0].to_json(force_ascii=False)
 
+
+@app.get('/predict')
+async def predict(request: Predict):
+    data = preprepict(request)
+    data_enc = encoding(data)
+    price = prediction(data_enc)
+    return {'price': price}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
--- api/preprocess.py
+++ api/preprocess.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import pickle
 import math
 
+with open('mode/model.pickle', 'rb') as file:
+    model_data = pickle.load(file)
 
-def preprocess(data) -> list | pd.DataFrame:
+def preparams(data) -> pd.DataFrame:
     tables = {
         "addresses": [],
         "developers": [],
@@ -10,139 +13,161 @@ def preprocess(data) -> list | pd.DataFrame:
         "offers_details": [],
         "realty_details": [],
         "realty_inside": [],
-        "realty_outside": []
-    }
+        "realty_outside": []}
 
     for table_name in tables.keys():
         if hasattr(data, table_name):
             tables[table_name].append(getattr(data, table_name).dict())
 
-    addresses_df = pd.DataFrame(tables["addresses"])
-    developers_df = pd.DataFrame(tables["developers"])
-    offers_df = pd.DataFrame(tables["offers"])
-    offers_details_df = pd.DataFrame(tables["offers_details"])
-    realty_details_df = pd.DataFrame(tables["realty_details"])
-    realty_inside_df = pd.DataFrame(tables["realty_inside"])
-    realty_outside_df = pd.DataFrame(tables["realty_outside"])
+    dfs = {table: pd.DataFrame(tables[table]) for table in tables}
 
-    addresses_df['lat'] = addresses_df['coordinates'].apply(lambda x: x['lat'] if isinstance(x, dict) else None)
-    addresses_df['lng'] = addresses_df['coordinates'].apply(lambda x: x['lng'] if isinstance(x, dict) else None)
-    addresses_df.drop(columns=['coordinates', 'address'], inplace=True)
+    data = dfs["addresses"].merge(dfs["offers"], on='cian_id', how='inner').merge(
+        dfs["offers_details"], on='cian_id', how='inner')
 
-    offers_df = offers_df.dropna(subset=['photos_count'])
-    offers_df['publication_at'] = pd.to_datetime(pd.to_datetime(
-        offers_df['publication_at'], unit='s', utc=True).dt.date)
-
-    offers_df = offers_df.dropna(subset=['photos_count'])
-    offers_df['publication_at'] = pd.to_datetime(pd.to_datetime(
-        offers_df['publication_at'], unit='s', utc=True).dt.date)
-
-    realty_details_df['finish_year'] = realty_details_df['finish_date'].apply(
-        lambda x: x.get('year') if isinstance(x, dict) else None)
-
-    realty_details_df['realty_type'] = realty_details_df['realty_type'].replace('none', None)
-    realty_details_df.loc[realty_details_df['finish_year'] <= 0, 'finish_year'] = None
-    realty_outside_df['material_type'] = realty_outside_df.get('material_type', None)
-    realty_outside_df['material_type'] = realty_outside_df['material_type'].replace('none', None)
-    realty_details_df.drop(columns=['finish_date'], inplace=True)
-
-    main_df = addresses_df.merge(offers_df, on='cian_id', how='inner').merge(offers_details_df, on='cian_id', how='inner')
-
-    tables_to_left_join = [developers_df, realty_details_df, realty_inside_df, realty_outside_df]
+    tables_to_left_join = [dfs["developers"], dfs["realty_details"],
+                           dfs["realty_inside"], dfs["realty_outside"]]
     for table in tables_to_left_join:
-        main_df = main_df.merge(table, on='cian_id', how='left')
+        data = data.merge(table, on='cian_id', how='left')
+    return data
 
-    main_df = main_df[main_df['photos_count'] >= 0].reset_index(drop=True)
 
-    main_df['separated_wc'] = main_df['separated_wc'].astype(float).fillna(0)
-    main_df['loggias'] = main_df['loggias'].astype(float).fillna(0)
-    main_df['balconies'] = main_df['balconies'].astype(float).fillna(0)
-    main_df['combined_wc'] = main_df['combined_wc'].astype(float).fillna(0)   
-    main_df['passenger_lifts'] = main_df['passenger_lifts'].astype(float).fillna(0)
+def preprepict(data) -> list | pd.DataFrame:
+    data = pd.DataFrame([data.dict()])
+    data['lat'] = data['coordinates'].apply(lambda x: x['lat'] if isinstance(x, dict) else None)
+    data['lng'] = data['coordinates'].apply(lambda x: x['lng'] if isinstance(x, dict) else None)
+    data.drop(columns=['coordinates', 'address'], inplace=True)
 
-    main_df['total_rate'] = main_df['total_rate'].astype(float).fillna(4.180479743602584)
-    main_df['review_count'] = main_df['review_count'].astype(float).fillna(1248.7316089939407)
+    data = data.dropna(subset=['photos_count'])
+    data['publication_at'] = pd.to_datetime(pd.to_datetime(data['publication_at'], unit='s', utc=True).dt.date)
+
+    data['finish_year'] = data['finish_date'].apply(lambda x: x.get('year') if isinstance(x, dict) else None)
+
+    data['realty_type'] = data['realty_type'].replace('none', None)
+    data.loc[data['finish_year'] <= 0, 'finish_year'] = None
+    data['material_type'] = data.get('material_type', None)
+    data['material_type'] = data['material_type'].replace('none', None)
+    data.drop(columns=['finish_date'], inplace=True)
+
+    data['separated_wc'] = data['separated_wc'].astype(float).fillna(0)
+    data['loggias'] = data['loggias'].astype(float).fillna(0)
+    data['balconies'] = data['balconies'].astype(float).fillna(0)
+    data['combined_wc'] = data['combined_wc'].astype(float).fillna(0)
+    data['passenger_lifts'] = data['passenger_lifts'].astype(float).fillna(0)
+
+    data['total_rate'] = data['total_rate'].astype(float).fillna(4.180479743602584)
+    data['review_count'] = data['review_count'].astype(float).fillna(1248.7316089939407)
 
     mean_proportion_ceiling_height = 0.06049278161032404
-    formula = main_df['total_area'] * mean_proportion_ceiling_height
-    main_df['ceiling_height'] = main_df['ceiling_height'].where(
-        main_df['ceiling_height'] > 0, formula)
+    formula = data['total_area'] * mean_proportion_ceiling_height
+    data['ceiling_height'] = data['ceiling_height'].where(data['ceiling_height'] > 0, formula)
 
     mean_proportion_living_area = 0.5486437974462534
-    main_df['living_area'] = main_df['living_area'].fillna(main_df['total_area'] * mean_proportion_living_area)
+    data['living_area'] = data['living_area'].fillna(data['total_area'] * mean_proportion_living_area)
 
     mean_proportion_kitchen_area = 0.4577734591774278
-    mask = (main_df['total_area'] - main_df['living_area']).replace(0, pd.NA) * mean_proportion_kitchen_area
-    main_df['kitchen_area'] = main_df['kitchen_area'].astype(float).fillna(mask)
+    mask = (data['total_area'] - data['living_area']
+            ).replace(0, pd.NA) * mean_proportion_kitchen_area
+    data['kitchen_area'] = data['kitchen_area'].astype(float).fillna(mask)
 
     mean_proportion_rooms_count = 0.06657494706605477
-    main_df['rooms_count'] = main_df['rooms_count'].fillna(main_df['living_area'] * mean_proportion_rooms_count).astype(int)
-    main_df['build_year'] = main_df.apply(lambda row: row['finish_year'] if pd.isna(row['build_year']) else row['build_year'], axis=1)
+    data['rooms_count'] = data['rooms_count'].fillna(data['living_area'] * mean_proportion_rooms_count).astype(int)
+    data['build_year'] = data.apply(lambda row: row['finish_year'] if pd.isna(row['build_year']) else row['build_year'], axis=1)
 
-    main_df = main_df.dropna(subset=['travel_time', 'views_count', 'kitchen_area', 'build_year']).copy()
+    data['is_penthouse'] = data['is_penthouse'].astype(bool).fillna(False)
+    data['garbage_chute'] = data['garbage_chute'].astype(bool).fillna(False)
+    data['is_emergency'] = data.get('is_emergency', None)
+    data['renovation_programm'] = data.get('renovation_programm', None)
+    data['project_type'] = data.get('project_type', None)
+    data['is_emergency'] = data['is_emergency'].astype(bool).fillna(False)
+    data['is_apartment'] = data['is_apartment'].astype(bool).fillna(False)
+    data['is_mortgage_allowed'] = data['is_mortgage_allowed'].astype(bool).fillna(False)
+    data['renovation_programm'] = data['renovation_programm'].astype(bool).fillna(False)
 
-    main_df['is_penthouse'] = main_df['is_penthouse'].astype(bool).fillna(False)
-    main_df['garbage_chute'] = main_df['garbage_chute'].astype(bool).fillna(False)
-    main_df['is_emergency'] = main_df.get('is_emergency', None)
-    main_df['renovation_programm'] = main_df.get('renovation_programm', None)
-    main_df['project_type'] = main_df.get('project_type', None)
-    main_df['is_emergency'] = main_df['is_emergency'].astype(bool).fillna(False)
-    main_df['is_apartment'] = main_df['is_apartment'].astype(bool).fillna(False)
-    main_df['is_mortgage_allowed'] = main_df['is_mortgage_allowed'].astype(bool).fillna(False)
-    main_df['renovation_programm'] = main_df['renovation_programm'].astype(bool).fillna(False)
+    data['photos_count'] = data['photos_count'].astype(int)
+    data['price'] = data['price'].astype(int)
+    data['travel_time'] = data['travel_time'].astype(int)
+    data['views_count'] = data['views_count'].astype(int)
+    data['balconies'] = data['balconies'].astype(int)
+    data['loggias'] = data['loggias'].astype(int)
+    data['separated_wc'] = data['separated_wc'].astype(int)
+    data['combined_wc'] = data['combined_wc'].astype(int)
+    data['passenger_lifts'] = data['passenger_lifts'].astype(int)
+    data['review_count'] = data['review_count'].astype(int)
+    data['build_year'] = data['build_year'].astype(int)
+    if data['material_type'] is None: data['material_type'] = 'ground'
+    if data['repair_type'] is None: data['repair_type'] = 'cosmetic'
 
-    main_df['photos_count'] = main_df['photos_count'].astype(int)
-    main_df['price'] = main_df['price'].astype(int)
-    main_df['travel_time'] = main_df['travel_time'].astype(int)
-    main_df['views_count'] = main_df['views_count'].astype(int)
-    main_df['balconies'] = main_df['balconies'].astype(int)
-    main_df['loggias'] = main_df['loggias'].astype(int)
-    main_df['separated_wc'] = main_df['separated_wc'].astype(int)
-    main_df['combined_wc'] = main_df['combined_wc'].astype(int)
-    main_df['passenger_lifts'] = main_df['passenger_lifts'].astype(int)
-    main_df['review_count'] = main_df['review_count'].astype(int)
-    main_df['build_year'] = main_df['build_year'].astype(int)
-    if main_df['material_type'] is None: main_df['material_type'] = 'ground'
-    if main_df['repair_type'] is None: main_df['repair_type'] = 'cosmetic'
+    data['publication_at'] = pd.to_datetime(data['publication_at'])
+    data['year'] = data['publication_at'].dt.year
+    data['month'] = data['publication_at'].dt.month
+    data['day_of_week'] = data['publication_at'].dt.dayofweek
+    data['day_of_month'] = data['publication_at'].dt.day
 
-    main_df['publication_at'] = pd.to_datetime(main_df['publication_at'])
-    main_df['year'] = main_df['publication_at'].dt.year
-    main_df['month'] = main_df['publication_at'].dt.month
-    main_df['day_of_week'] = main_df['publication_at'].dt.dayofweek
-    main_df['day_of_month'] = main_df['publication_at'].dt.day
-
-    main_df = main_df.drop(columns=['publication_at'])
+    data = data.drop(columns=['publication_at'])
 
     center_lat = 55.753600
     center_lng = 37.621184
     earth_radius_km = 6371
+
     def haversine(lat1, lng1, lat2, lng2):
         lat1, lng1, lat2, lng2 = map(math.radians, [lat1, lng1, lat2, lng2])
         dlat = lat2 - lat1
         dlng = lng2 - lng1
-        a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlng / 2)**2
+        a = math.sin(dlat / 2)**2 + math.cos(lat1) * \
+            math.cos(lat2) * math.sin(dlng / 2)**2
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
         return earth_radius_km * c
 
-    main_df['distance_from_center'] = main_df.apply(
-        lambda row: haversine(row['lat'], row['lng'], center_lat, center_lng), axis=1)
+    data['distance_from_center'] = data.apply(lambda row: haversine(row['lat'], row['lng'], center_lat, center_lng), axis=1)
+    return data
 
-    columns_to_keep = ['county', 'district', 'metro', 'travel_type', 'travel_time', 'price',
-                    'category', 'views_count', 'photos_count', 'floor_number',
-                    'floors_count', 'flat_type', 'sale_type', 'review_count', 'total_rate',
-                    'project_type', 'is_apartment', 'is_penthouse', 'is_mortgage_allowed',
-                    'is_premium', 'is_emergency', 'renovation_programm', 'repair_type',
-                    'total_area', 'living_area', 'kitchen_area', 'ceiling_height',
-                    'balconies', 'loggias', 'rooms_count', 'separated_wc', 'combined_wc',
-                    'build_year', 'material_type', 'garbage_chute', 'passenger_lifts',
-                    'distance_from_center', 'year', 'month', 'day_of_week', 'day_of_month']
+def encoding(data):
+    #onehot
+    onehot_columns = model_data['onehot_encoder'].feature_names_in_
+    for col in onehot_columns:
+        if col not in data.columns:
+            raise ValueError(f"Признак '{col}' отсутствует в данных")
+        if data[col].isnull().any():
+            raise ValueError(f"Признак '{col}' пустой")
+    data_encoded = pd.DataFrame(model_data['onehot_encoder'].transform(data[onehot_columns]), columns=model_data['onehot_encoder'].get_feature_names_out(onehot_columns))
+    data = pd.concat([data.drop(columns=onehot_columns).reset_index(drop=True), data_encoded], axis=1)
 
-    main_df = main_df[columns_to_keep]
+    # origin
+    if data.get('repair_type') is None: raise ValueError("Признак 'repair_type' пустой")
+    orignal_columns = {'repair_type': {'no': 0, 'cosmetic': 1, 'euro': 2, 'design': 3}}
+    for col, mapping in orignal_columns.items():
+        data[col] = data[col].map(mapping)
+    
+    # target
+    target_columns = ['district', 'project_type', 'metro']
+    for col in target_columns:
+        if col not in data.columns:
+            raise ValueError(f"Признак '{col}' отсутствует в данных")
+        if data[col].isnull().any():
+            raise ValueError(f"Признак '{col}' пустой")
+    data[target_columns] = pd.DataFrame(model_data['target_encoder'].transform(data[target_columns]), columns=target_columns)
 
+    # scaler
+    scaler_columns = model_data['scaler'].feature_names_in_
+    for col in scaler_columns:
+        if col not in data.columns:
+            raise ValueError(f"Признак '{col}' отсутствует в данных")
+        if data[col].isnull().any():
+            raise ValueError(f"Признак '{col}' пустой")
+    data = data[scaler_columns]
+    data = pd.DataFrame(model_data['scaler'].transform(data), columns=data.columns)
 
-    missing_columns = main_df[['district', 'county', 'sale_type']].isna().any()
-    if missing_columns.any():
-        return missing_columns[missing_columns].index.to_list()
+    # model
+    model_columns = [col for col in model_data['model'].feature_names_in_ if col in data.columns]
+    for col in model_columns:
+        if col not in data.columns:
+            raise ValueError(f"Признак '{col}' отсутствует в данных")
+        if data[col].isnull().any():
+            raise ValueError(f"Признак '{col}' пустой")
+    data = data[model_columns]
+    return data
 
-    X = main_df.drop(columns=['price'])
-    return X
+def prediction(data):
+    # predict
+    predict = model_data['model'].predict(data)
+    return predict[0]
