11a69f32c359edec7b11733dc59f586baf1cb9a4
George Lavrenov <lavrenovg471@gmail.com>
2024-12-30 15:57:27 +0300

    Проверил  код с помощью flake 8

---
+++ fastapi.py
@@ -0,0 +1,32 @@
+from .preprocess import preparams, preprepict, encoding, prediction
+from fastapi import FastAPI, HTTPException
+from app.main import apartPage
+from .models import Predict, Params, PredictResponse
+import uvicorn
+import re
+
+app = FastAPI()
+
+
+@app.get('/getparams', response_model=Predict)
+async def getparams(url: str):
+    match = re.search(r'flat/(\d{4,})', url)
+    if not match or not (id := match.group(1)):
+        raise HTTPException(status_code=400, detail='Неверный формат объявления')
+    data = apartPage([id], dbinsert=0)
+    data = Params(**data)
+    response = preparams(data)
+    return response
+
+
+@app.post('/predict', response_model=PredictResponse)
+async def predict(request: Predict):
+    data = preprepict(request)
+    data_enc = encoding(data)
+    if isinstance(data_enc, ValueError):
+        raise HTTPException(status_code=400, detail=str(data_enc))
+    price = prediction(data_enc)
+    return {'price': price}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
