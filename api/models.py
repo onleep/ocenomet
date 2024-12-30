b1f38d04b18fd9c1b7276e64f9d4bf5e15795d50
onleep <120320355+onleep@users.noreply.github.com>
2024-12-30 17:33:08 +0300

    Update models default values

--- api/models.py
+++ api/models.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel
 class Offers(BaseModel):
     cian_id: int
     price: float
-    category: str | None = None
-    views_count: int | None = None
-    photos_count: int | None = None
+    category: str | None = 'flatSale'
+    views_count: int | None = 632
+    photos_count: int | None = 18
     floor_number: int | None = None
     floors_count: int | None = None
     publication_at: int | None = None
@@ -28,16 +28,16 @@ class Addresses(BaseModel):
 
 class RealtyInside(BaseModel):
     cian_id: int
-    repair_type: str | None = None
+    repair_type: str | None = 'cosmetic'
     total_area: float | None = None
     living_area: float | None = None
     kitchen_area: float | None = None
     ceiling_height: float | None = None
-    balconies: int | None = None
-    loggias: int | None = None
+    balconies: int | None = 0
+    loggias: int | None = 0
     rooms_count: int | None = None
-    separated_wc: float | None = None
-    combined_wc: int | None = None
+    separated_wc: float | None = 0
+    combined_wc: int | None = 0
     windows_view: str | None = None
 
 
@@ -45,26 +45,26 @@ class RealtyOutside(BaseModel):
     cian_id: int
     build_year: int | None = None
     entrances: int | None = None
-    material_type: str | None = None
+    material_type: str | None = 'panel'
     parking_type: str | None = None
-    garbage_chute: bool | None = None
+    garbage_chute: bool | None = False
     lifts_count: int | None = None
-    passenger_lifts: int | None = None
+    passenger_lifts: int | None = 0
     cargo_lifts: int | None = None
 
 
 class RealtyDetails(BaseModel):
     cian_id: int
     realty_type: str | None = None
-    project_type: str | None = None
+    project_type: str | None = 'Индивидуальный проект'
     heat_type: str | None = None
     gas_type: str | None = None
-    is_apartment: bool | None = None
-    is_penthouse: bool | None = None
-    is_mortgage_allowed: bool | None = None
-    is_premium: bool | None = None
-    is_emergency: bool | None = None
-    renovation_programm: bool | None = None
+    is_apartment: bool | None = False
+    is_penthouse: bool | None = False
+    is_mortgage_allowed: bool | None = False
+    is_premium: bool | None = False
+    is_emergency: bool | None = False
+    renovation_programm: bool | None = False
     finish_date: dict | None = None
 
 
@@ -73,7 +73,7 @@ class OffersDetails(BaseModel):
     agent_name: str | None = None
     deal_type: str | None = None
     flat_type: str | None = None
-    sale_type: str | None = None
+    sale_type: str | None = 'free'
     # is_duplicate: bool | None = None
     description: str | None = None
 
--- api/preprocess.py
+++ api/preprocess.py
@@ -5,6 +5,22 @@ import math
 with open('model/model.pickle', 'rb') as file:
     model_data = pickle.load(file)
 
+def distance_from_center(data) -> pd.DataFrame:
+    data['lat'] = data['coordinates'].apply(lambda x: x['lat'] if isinstance(x, dict) else None)
+    data['lng'] = data['coordinates'].apply(lambda x: x['lng'] if isinstance(x, dict) else None)
+    center_lat = 55.753600
+    center_lng = 37.621184
+    earth_radius_km = 6371
+    def haversine(lat1, lng1, lat2, lng2):
+        lat1, lng1, lat2, lng2 = map(math.radians, [lat1, lng1, lat2, lng2])
+        dlat = lat2 - lat1
+        dlng = lng2 - lng1
+        a = math.sin(dlat / 2)**2 + math.cos(lat1) * \
+            math.cos(lat2) * math.sin(dlng / 2)**2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        return earth_radius_km * c
+    data['distance_from_center'] = data.apply(lambda row: haversine(row['lat'], row['lng'], center_lat, center_lng), axis=1)
+    return data
 
 def preparams(data) -> dict:
     tables = {
@@ -26,29 +42,13 @@ def preparams(data) -> dict:
         dfs["offers_details"], on='cian_id', how='inner')
 
     tables_to_left_join = [dfs["developers"], dfs["realty_details"],
-                           dfs["realty_inside"], dfs["realty_outside"]]
+                        dfs["realty_inside"], dfs["realty_outside"]]
     for table in tables_to_left_join:
         data = data.merge(table, on='cian_id', how='left')
 
     # расчитываем дистанцию от центра
-    data['lat'] = data['coordinates'].apply(lambda x: x['lat'] if isinstance(x, dict) else None)
-    data['lng'] = data['coordinates'].apply(lambda x: x['lng'] if isinstance(x, dict) else None)
-    center_lat = 55.753600
-    center_lng = 37.621184
-    earth_radius_km = 6371
-
-    def haversine(lat1, lng1, lat2, lng2):
-        lat1, lng1, lat2, lng2 = map(math.radians, [lat1, lng1, lat2, lng2])
-        dlat = lat2 - lat1
-        dlng = lng2 - lng1
-        a = math.sin(dlat / 2)**2 + math.cos(lat1) * \
-            math.cos(lat2) * math.sin(dlng / 2)**2
-        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-        return earth_radius_km * c
-    data['distance_from_center'] = data\
-        .apply(lambda row: haversine(row['lat'], row['lng'], center_lat, center_lng), axis=1)
+    data = distance_from_center(data)
     return data.iloc[0].to_dict()
-
 
 def preprepict(data) -> pd.DataFrame:
     data = pd.DataFrame([data.dict()])
@@ -56,7 +56,6 @@ def preprepict(data) -> pd.DataFrame:
     data['lng'] = data['coordinates'].apply(lambda x: x['lng'] if isinstance(x, dict) else None)
     data.drop(columns=['coordinates'], inplace=True)
 
-    data = data.dropna(subset=['photos_count'])
     data['publication_at'] = pd.to_datetime(pd.to_datetime(data['publication_at'], unit='s', utc=True).dt.date)
 
     data['finish_year'] = data['finish_date'].apply(lambda x: x.get('year') if isinstance(x, dict) else None)
@@ -66,12 +65,6 @@ def preprepict(data) -> pd.DataFrame:
     data['material_type'] = data['material_type'].replace('none', None)
     data.drop(columns=['finish_date'], inplace=True)
 
-    data['separated_wc'] = data['separated_wc'].astype(float).fillna(0)
-    data['loggias'] = data['loggias'].astype(float).fillna(0)
-    data['balconies'] = data['balconies'].astype(float).fillna(0)
-    data['combined_wc'] = data['combined_wc'].astype(float).fillna(0)
-    data['passenger_lifts'] = data['passenger_lifts'].astype(float).fillna(0)
-
     data['total_rate'] = data['total_rate'].astype(float).fillna(4.180479743602584)
     data['review_count'] = data['review_count'].astype(float).fillna(1248.7316089939407)
 
@@ -80,40 +73,23 @@ def preprepict(data) -> pd.DataFrame:
     data['ceiling_height'] = data['ceiling_height'].where(data['ceiling_height'] > 0, formula)
 
     mean_proportion_living_area = 0.5486437974462534
-    data['living_area'] = data['living_area'].fillna(data['total_area'] * mean_proportion_living_area)
+    data['living_area'] = data['living_area'].astype(float).fillna(data['total_area'] * mean_proportion_living_area)
 
     mean_proportion_kitchen_area = 0.4577734591774278
     mask = (data['total_area'] - data['living_area']
             ).replace(0, pd.NA) * mean_proportion_kitchen_area
     data['kitchen_area'] = data['kitchen_area'].astype(float).fillna(mask)
-
     mean_proportion_rooms_count = 0.06657494706605477
-    data['rooms_count'] = data['rooms_count'].fillna(data['living_area'] * mean_proportion_rooms_count)\
-        .astype(int)
-    data['build_year'] = data\
-        .apply(lambda row: row['finish_year'] if pd.isna(row['build_year']) else row['build_year'], axis=1)
+    data['rooms_count'] = data['rooms_count'].fillna(data['living_area'] * mean_proportion_rooms_count).astype(int)
+    data['build_year'] = data.apply(lambda row: row['finish_year'] if pd.isna(row['build_year']) else row['build_year'], axis=1)
 
-    data['is_penthouse'] = data['is_penthouse'].astype(bool).fillna(False)
-    data['garbage_chute'] = data['garbage_chute'].astype(bool).fillna(False)
-    data['is_emergency'] = data['is_emergency'].astype(bool).fillna(False)
-    data['is_apartment'] = data['is_apartment'].astype(bool).fillna(False)
-    data['is_mortgage_allowed'] = data['is_mortgage_allowed'].astype(bool).fillna(False)
-    data['renovation_programm'] = data['renovation_programm'].astype(bool).fillna(False)
-    data['material_type'] = data['material_type'].fillna('panel')
-    data['repair_type'] = data['repair_type'].fillna('cosmetic')
-    data['project_type'] = data['project_type'].fillna('Индивидуальный проект')
+    columns_to_int = [
+        'price', 'travel_time', 'views_count', 'balconies', 'loggias', 'photos_count',
+        'separated_wc', 'combined_wc', 'passenger_lifts', 'review_count', 'build_year'
+    ]
 
-    data['photos_count'] = data['photos_count'].astype(int)
-    data['price'] = data['price'].astype(int)
-    data['travel_time'] = data['travel_time'].astype(int)
-    data['views_count'] = data['views_count'].astype(int)
-    data['balconies'] = data['balconies'].astype(int)
-    data['loggias'] = data['loggias'].astype(int)
-    data['separated_wc'] = data['separated_wc'].astype(int)
-    data['combined_wc'] = data['combined_wc'].astype(int)
-    data['passenger_lifts'] = data['passenger_lifts'].astype(int)
-    data['review_count'] = data['review_count'].astype(int)
-    data['build_year'] = data['build_year'].astype(int)
+    for col in columns_to_int:
+        data[col] = data[col].apply(lambda x: int(x) if pd.notna(x) else None)
 
     data['publication_at'] = pd.to_datetime(data['publication_at'])
     data['year'] = data['publication_at'].dt.year
@@ -122,27 +98,10 @@ def preprepict(data) -> pd.DataFrame:
     data['day_of_month'] = data['publication_at'].dt.day
 
     data = data.drop(columns=['publication_at'])
-
-    center_lat = 55.753600
-    center_lng = 37.621184
-    earth_radius_km = 6371
-
-    def haversine(lat1, lng1, lat2, lng2):
-        lat1, lng1, lat2, lng2 = map(math.radians, [lat1, lng1, lat2, lng2])
-        dlat = lat2 - lat1
-        dlng = lng2 - lng1
-        a = math.sin(dlat / 2)**2 + math.cos(lat1) * \
-            math.cos(lat2) * math.sin(dlng / 2)**2
-        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-        return earth_radius_km * c
-
-    data['distance_from_center'] = data\
-        .apply(lambda row: haversine(row['lat'], row['lng'], center_lat, center_lng), axis=1)
     return data
 
-
 def encoding(data):
-    # onehot
+    #onehot
     onehot_columns = model_data['onehot_encoder'].feature_names_in_
     for col in onehot_columns:
         if col not in data.columns:
@@ -154,12 +113,11 @@ def encoding(data):
     data = pd.concat([data.drop(columns=onehot_columns).reset_index(drop=True), data_encoded], axis=1)
 
     # origin
-    if data.get('repair_type') is None:
-        return ValueError("Признак 'repair_type' пустой")
+    if data.get('repair_type') is None: return ValueError("Признак 'repair_type' пустой")
     orignal_columns = {'repair_type': {'no': 0, 'cosmetic': 1, 'euro': 2, 'design': 3}}
     for col, mapping in orignal_columns.items():
         data[col] = data[col].map(mapping)
-
+    
     # target
     target_columns = ['district', 'project_type', 'metro']
     for col in target_columns:
@@ -167,8 +125,7 @@ def encoding(data):
             return ValueError(f"Признак '{col}' отсутствует в данных")
         if data[col].isnull().any():
             return ValueError(f"Признак '{col}' пустой")
-    data[target_columns] = pd.DataFrame(model_data['target_encoder'].transform(data[target_columns]),
-                                        columns=target_columns)
+    data[target_columns] = pd.DataFrame(model_data['target_encoder'].transform(data[target_columns]), columns=target_columns)
 
     # scaler
     scaler_columns = model_data['scaler'].feature_names_in_
@@ -189,7 +146,6 @@ def encoding(data):
             return ValueError(f"Признак '{col}' пустой")
     data = data[model_columns]
     return data
-
 
 def prediction(data) -> float:
     # predict
