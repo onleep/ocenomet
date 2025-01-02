23a39726643ca368fdc58d8de9b39cd4c27dbb81
Simon <92649440+krabat7@users.noreply.github.com>
2025-01-02 18:10:31 +0300

    Add streamlit (#13)
    Co-authored-by: Simon Cheremisin <semion.cheremisin@gmail>

---
+++ streamlit/api_client.py
@@ -0,0 +1,137 @@
+import httpx
+import numpy as np
+import json
+import streamlit as st
+
+# Получение данных с объявления Циан
+def get_data_page(url):
+    """Получает данные с объявления Циан"""
+    endpoint = "http://62.113.119.220:8000/api/getparams"
+    try:
+        response = httpx.get(endpoint, params={'url': url}, timeout=120)
+        response.raise_for_status()
+        return response
+    except Exception as e:
+        return f"Произошла ошибка при запросе: {e}", None
+
+# Получение предсказанной стоимости от модели
+def get_predict_price(input_data):
+    """Получает предсказанную цену от модели"""
+    endpoint = "http://62.113.119.220:8000/api/predict"
+    try:
+        response = httpx.post(endpoint, json={'data': input_data})
+        response.raise_for_status()
+        data = response.json()
+
+        return float(data["price"])
+    except Exception as e:
+        return f"Ошибка: {e}"
+
+# Подготовка данных для отправки на сервер
+def prepare_data(X, y):
+    """Преобразует данные X и y в ожидаемый формат"""
+    X_prepared = [
+        {f"feature_{i+1}": float(value) for i, value in enumerate(row)}
+        for row in X
+    ]
+    
+    y_prepared = [float(value) for value in y]
+    
+    return X_prepared, y_prepared
+
+# Обучение модели
+def fit_model(model_id, model_type, hyperparameters, X, y):
+    """Создает и обучает модель"""
+    endpoint = "http://62.113.119.220:8000/api/fit"
+
+    config = {
+        "id": model_id,
+        "ml_model_type": model_type,
+        "hyperparameters": hyperparameters
+    }
+    
+    X_prepared, y_prepared = prepare_data(X, y)
+    print("Prepared X:", X_prepared)
+    print("Prepared y:", y_prepared)
+
+    payload = [
+        {
+            "X": X_prepared,
+            "y": y_prepared,
+            "config": config
+        }
+    ]
+    print("Payload sent to server:")
+    print(json.dumps(payload, indent=2))
+
+    try:
+        response = httpx.post(endpoint, json=payload, timeout=300)
+        print("Response status code:", response.status_code)
+        print("Response text:", response.text)
+        response.raise_for_status()
+        return response.json()
+    except httpx.HTTPStatusError as e:
+        st.error(f"Детали ошибки: {e.response.text}")
+        return f"Ошибка при обучении модели: {e.response.status_code}"
+    except Exception as e:
+        st.error(f"Неизвестная ошибка: {e}")
+        return f"Ошибка: {e}"
+
+# Получение списка всех моделей
+def list_models():
+    """
+    Получает список всех моделей с подробной информацией.
+    """
+    endpoint = "http://62.113.119.220:8000/api/list_models"
+    try:
+        response = httpx.get(endpoint, timeout=60)
+        response.raise_for_status()
+        models = response.json()
+        print(models)
+        return models
+    except Exception as e:
+        return f"Ошибка при получении списка моделей: {e}"
+
+# Загрузка модели
+def load_model(model_id):
+    """Загружает модель"""
+    endpoint = "http://62.113.119.220:8000/api/load"
+    try:
+        response = httpx.post(endpoint, json={"id": model_id}, timeout=60)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        return f"Ошибка при загрузке модели: {e}"
+
+# Выгрузка всех моделей
+def unload_model():
+    """Выгружает все модели"""
+    endpoint = "http://62.113.119.220:8000/api/unload"
+    try:
+        response = httpx.post(endpoint, timeout=60)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        return f"Ошибка при выгрузке моделей: {e}"
+
+# Удаление конкретной модели
+def remove_model(model_id):
+    """Удаляет модель"""
+    endpoint = f"http://62.113.119.220:8000/api/remove/{model_id}"
+    try:
+        response = httpx.delete(endpoint, timeout=60)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        return f"Ошибка при удалении модели: {e}"
+
+# Удаление всех моделей
+def remove_all_models():
+    """Удаляет все модели"""
+    endpoint = "http://62.113.119.220:8000/api/remove_all"
+    try:
+        response = httpx.delete(endpoint, timeout=60)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        return f"Ошибка при удалении всех моделей: {e}"
---
+++ streamlit/mapping_utils.py
@@ -0,0 +1,215 @@
+value_mappings = {
+    'material_type': {
+        'Панельный': 'panel',
+        'Монолитный': 'monolith',
+        'Блочный': 'block',
+        'Кирпичный': 'brick',
+        'Монолитно-кирпичный': 'monolithBrick',
+        'Сталинский': 'stalin',
+        'Деревянный': 'wood'
+    },
+    'flat_type': {
+        'Комнатная квартира': 'rooms',
+        'Студия': 'studio',
+        'Свободная планировка': 'openPlan'
+    },
+    'repair_type': {
+        'Евроремонт': 'euro',
+        'Без ремонта': 'no',
+        'Косметический': 'cosmetic',
+        'Дизайнерский': 'design'
+    },
+    'travel_type': {
+        'Пешком': 'walk',
+        'На транспорте': 'transport'
+    },
+    'category': {
+        'Квартира в новостройке': 'newBuildingFlatSale',
+        'Доля в квартире': 'flatShareSale',
+        'Квартира': 'flatSale'
+    },
+    'sale_type': {
+        'Свободная продажа': 'free',
+        '214-ФЗ': 'fz214',
+        'Альтернатива': 'alternative',
+        'Договор уступки права требования': 'dupt',
+        'Предварительный договор купли-продажи': 'pdkp',
+        'Договор ЖСК': 'dzhsk',
+        'Договор инвестирования': 'investment'
+    },
+    'deal_type': {
+        'Продажа': 'sale'
+    },
+    'parking_type': {
+        'Наземная': 'ground',
+        'Подземная': 'underground',
+        'Многоуровневая': 'multilevel',
+        'Открытая': 'open',
+        'На крыше': 'roof'
+    },
+    'windows_view': {
+        'На улицу и двор': 'yardAndStreet',
+        'Во двор': 'yard',
+        'На улицу': 'street'
+    },
+    'realty_type': {
+        'Квартира': 'flat'
+    },
+    'heat_type': {
+        'Центральное': 'central',
+        'ИТП': 'itp',
+        'Автоманомный котел': 'autonomousBoiler',
+        'Котел': 'boiler',
+        'Электрическое': 'electric',
+        'Без отопления': 'without',
+        'Печь': 'stove'
+    },
+    'gas_type': {
+        'Центральное': 'central',
+        'Автономное': 'autonomous',
+        'Без водоснабжения': 'autonomous',
+        'Неизвестно': 'unknown'
+    }
+}
+
+column_mapping = {
+    'Округ': 'county',
+    'Район': 'district',
+    'Метро': 'metro',
+    'Способ передвижения': 'travel_type',
+    'Время до метро (мин)': 'travel_time',
+    'Категория': 'category',
+    'Кол-во просмотров': 'views_count',
+    'Кол-во фотографий': 'photos_count',
+    'Этаж': 'floor_number',
+    'Кол-во этажей': 'floors_count',
+    'Тип жилья': 'flat_type',
+    'Тип продажи': 'sale_type',
+    'Кол-во отзывов застройщика': 'review_count',
+    'Рейтинг застройщика ': 'total_rate',
+    'Тип проекта': 'project_type',
+    'Апартаменты': 'is_apartment',
+    'Пентхаус': 'is_penthouse',
+    'Ипотека разрешена': 'is_mortgage_allowed',
+    'Премиум': 'is_premium',
+    'Аварийное состояние': 'is_emergency',
+    'Реновация': 'renovation_programm',
+    'Тип ремонта': 'repair_type',
+    'Общая площадь (м²)': 'total_area',
+    'Жилая площадь (м²)': 'living_area',
+    'Площадь кухни (м²)': 'kitchen_area',
+    'Высота потолков (м)': 'ceiling_height',
+    'Кол-во балконов': 'balconies',
+    'Кол-во лоджий': 'loggias',
+    'Кол-во комнат': 'rooms_count',
+    'Раздельный санузел': 'separated_wc',
+    'Совмещённый санузел': 'combined_wc',
+    'Год постройки': 'build_year',
+    'Тип материала': 'material_type',
+    'Мусоропровод': 'garbage_chute',
+    'Расстояние до центра (км)': 'distance_from_center',
+    'Год публикации': 'year',
+    'Месяц публикации': 'month',
+    'День недели публикации': 'day_of_week',
+    'День месяца публикации': 'day_of_month',
+    'Стоимость': 'price',
+    'Название застройщика': 'name',
+    'Кол-во зданий застройщика': 'buildings_count',
+    'Год основания застройщика': 'foundation_year',
+    'Надежность застройщика': 'is_reliable',
+    'Название агентства': 'agent_name',
+    'Описание': 'description',
+    'Тип недвижимости': 'realty_type',
+    'Тип отопления': 'heat_type',
+    'Тип газа': 'gas_type',
+    'Кол-во подъездов': 'entrances',
+    'Тип парковки': 'parking_type',
+    'Кол-во лифтов': 'lifts_count',
+    'Грузовые лифты': 'cargo_lifts',
+    'Вид из окон': 'windows_view',
+    'Координаты (широта)': 'coordinates.lat',
+    'Координаты (долгота)': 'coordinates.lng',
+    'Тип сделки': 'deal_type',
+    'id объявления': 'cian_id',
+    'Улица': 'street',
+    'Дом': 'house',
+    'Кол-во пассажирских лифтов': 'passenger_lifts',
+    'Дата публикации': 'publication_at',
+    'Дата завершения строительства': 'finish_date'
+}
+
+column_order = [
+    # Основные параметры
+    "Стоимость", "Общая площадь (м²)", "Жилая площадь (м²)", "Площадь кухни (м²)", "Кол-во комнат", 
+    "Этаж", "Кол-во этажей", "Год постройки",
+
+    # Местоположение
+    "Метро", "Время до метро (мин)", "Округ", "Район", "Расстояние до центра (км)",
+
+    # Тип и состояние жилья
+    "Тип жилья", "Тип материала", "Тип ремонта", "Способ передвижения", "Категория",
+
+    # Дополнительные характеристики
+    "Тип сделки", "Кол-во просмотров", "Кол-во фотографий", "Кол-во отзывов застройщика", "Рейтинг застройщика ", 
+    "Ипотека разрешена", "Премиум", "Аварийное состояние", "Реновация", 
+    "Пентхаус", "Апартаменты",
+
+    # Информация о доме
+    "Кол-во подъездов", "Тип парковки", "Кол-во лифтов", "Кол-во пассажирских лифтов", "Грузовые лифты", "Вид из окон",
+
+    # Инфраструктура и удобства
+    "Кол-во балконов", "Кол-во лоджий", "Мусоропровод", "Высота потолков (м)", "Раздельный санузел", "Совмещённый санузел",
+
+    # Параметры застройщика и агентства
+    "Название застройщика", "Кол-во зданий застройщика", "Год основания застройщика", 
+    "Надежность застройщика", "Название агентства", "Описание",
+
+    # Технические характеристики
+    "Тип недвижимости", "Тип отопления", "Тип газа", "Координаты (широта)", "Координаты (долгота)",
+
+    # Дата публикации
+    "Год публикации", "Месяц публикации", "День недели публикации", "День месяца публикации", "Дата публикации",
+
+    # Прочие параметры
+    "id объявления", "Улица", "Дом"
+]
+
+
+def map_values(material_type, flat_type, repair_type, travel_type):
+    return {
+        "material_type": value_mappings["material_type"].get(material_type, material_type),
+        "flat_type": value_mappings["flat_type"].get(flat_type, flat_type),
+        "repair_type": value_mappings["repair_type"].get(repair_type, repair_type),
+        "travel_type": value_mappings["travel_type"].get(travel_type, travel_type), 
+    }
+
+def map_dataframe(df, direction):
+    if direction not in {"to_english", "to_russian"}:
+        raise ValueError("Invalid direction. Use 'to_english' or 'to_russian'.")
+
+    if direction == "to_english":
+        df = df.rename(columns=column_mapping)
+
+    for column in df.columns:
+        if column in value_mappings:
+            mapping = value_mappings[column]
+
+            if direction == "to_english":
+                selected_mapping = mapping
+            else:
+                reverse_mapping = {v: k for k, v in mapping.items()}
+                selected_mapping = reverse_mapping
+
+            df[column] = df[column].map(selected_mapping).fillna(df[column])
+    
+    if direction == "to_russian":
+        reversed_column_mapping = {value: key for key, value in column_mapping.items()}
+        df = df.rename(columns=reversed_column_mapping)
+    return df
+
+def reorder_columns(df):
+    valid_columns = [col for col in column_order if col in df.columns]
+    remaining_columns = [col for col in df.columns if col not in valid_columns]
+    new_column_order = valid_columns + remaining_columns
+    
+    return df[new_column_order]
